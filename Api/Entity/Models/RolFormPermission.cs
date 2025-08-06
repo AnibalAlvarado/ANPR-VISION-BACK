28f0462132fbbc3f66457d426e63ac15d759937b
Anibal <wigetta315@gmail.com>
2025-08-05 19:14:46 -0500

    initial commit

---
+++ Api/Entity/Models/RolFormPermission.cs
@@ -0,0 +1,25 @@
+﻿using Entity.Annotations;
+using Entity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Model
+{
+    public class RolFormPermission : BaseModel
+    {
+        public int RolId { get; set; }
+        public int FormId { get; set; }
+        public int PermissionId { get; set; }
+
+        [ForeignInclude("Name", "Description")]
+        public Rol Rol { get; set; }
+        [ForeignInclude("Name", "Description")]
+        public Form Form { get; set; }
+        [ForeignInclude("Name")]
+        public Permission Permission { get; set; }
+
+    }
+}
