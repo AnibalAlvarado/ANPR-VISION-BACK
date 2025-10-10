c51ddbe13cf049f592fa263676bc93b0f77d3448
nataliosorio <nataliaosorio973@gmail.com>
2025-10-10 01:11:11 -0500

    dashboard-termiando-por-parkingId

--- Api/Business/Interfaces/Dashboard/IDashboardBusiness.cs
+++ Api/Business/Interfaces/Dashboard/IDashboardBusiness.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Entity.Dtos.Dashboard;
 
 namespace Business.Interfaces.Dashboard
 {
     public interface IDashboardBusiness
     {
-        Task<int> GetCurrentParkedCountAsync(CancellationToken ct = default);
+        Task<int> GetTotalCurrentlyParkedByParkingAsync(int parkingId);
+        Task<int> GetTotalCurrentlyParkedGlobalAsync();
+        Task<VehicleTypeDistributionDto> GetVehicleTypeDistributionGlobalAsync(int parkingId,bool includeZeros = true);
+        Task<List<OccupancyItemDto>> GetSectorOccupancyByZoneAsync(int zoneId);
+
+        Task<OccupancyDto> GetOccupancyGlobalAsync(int parkingId);
     }
 }
--- Api/Data/Interfaces/Dashboard/IDashboardRepository.cs
+++ Api/Data/Interfaces/Dashboard/IDashboardRepository.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Entity.Dtos.Dashboard;
 
 namespace Data.Interfaces.Dashboard
 {
     public interface IDashboardRepository
     {
-        /// <summary>
-        /// Cantidad de vehículos actualmente estacionados (ExitDate == null y no eliminados).
-        /// </summary>
-        Task<int> GetCurrentParkedCountAsync(CancellationToken ct = default);
+ 
+
+        Task<int> GetTotalCurrentlyParkedByParkingAsync(int parkingId);
+        Task<int> GetTotalCurrentlyParkedGlobalAsync();
+        Task<VehicleTypeDistributionDto> GetVehicleTypeDistributionByParkingAsync(int parkingId, bool includeZeros = true);
+        Task<List<OccupancyItemDto>> GetSectorOccupancyByZoneAsync(int zoneId);
+        Task<OccupancyDto> GetOccupancyGlobalAsync(int parkingId);
+
     }
 }
